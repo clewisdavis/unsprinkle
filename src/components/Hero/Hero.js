9c6cff1c5f7100ef949ad3c1caed322eb586e537
Chris Davis <chrislewis.davis@gmail.com>
2021-12-15 08:57:24 -0500

    exercise

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,26 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcset={`
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x,
+          `}
+        />
+        <source
+          type="image/jpeg"
+          srcset={`
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x,
+          `}
+        />
+      </picture>
+      <HeroImage alt="Example of photos on this site, cat on background" src="/images/hero-img.jpg" />
+      <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
   );
 };
